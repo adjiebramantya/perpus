4db35015e0f69a5a00b8cf9d1617345738cfda1f
adjiebramantya <adjiebramantya.ab@gmail.com>
2021-11-07 14:51:20 +0700

    2
    login dan database

--- application/views/v_login.php
+++ application/views/v_login.php
@@ -31,13 +31,15 @@
   <div class="login-box-body">
     <p class="login-box-msg login">Login Sistem</p>
 
-    <form action="../../index2.html" method="post">
+    <?= $this->session->flashdata('info');?>
+
+    <form action="<?= base_url()?>login/proses_login" method="post">
       <div class="form-group has-feedback">
-        <input type="text" name="username" class="form-control" placeholder="Silahkan Masukkan username">
+        <input type="text" name="username" class="form-control" placeholder="Silahkan Masukkan username" required>
         <span class="glyphicon glyphicon-user form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" id="password" name="password" class="form-control" placeholder="Silahkan Masukkan password">
+        <input type="password" id="password" name="password" class="form-control" placeholder="Silahkan Masukkan password" required>
         <div class="show-password">
           <span class="glyphicon glyphicon-lock form-control-feedback"></span>
         </div>
@@ -45,7 +47,7 @@
       <div class="row">
         <!-- /.col -->
         <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Login</button>
         </div>
         <!-- /.col -->
       </div>
