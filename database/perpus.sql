0ec101c2088cbaba8a85635644506e25e0eb76f3
adjiebramantya <adjiebramantya.ab@gmail.com>
2021-11-16 19:20:42 +0700

    upd db

--- database/perpus.sql
+++ database/perpus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 07 Nov 2021 pada 08.52
+-- Waktu pembuatan: 16 Nov 2021 pada 13.20
 -- Versi server: 10.1.39-MariaDB
 -- Versi PHP: 7.3.5
 
@@ -21,6 +21,30 @@ SET time_zone = "+00:00";
 --
 -- Database: `perpus`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `anggota`
+--
+
+CREATE TABLE `anggota` (
+  `id_anggota` varchar(10) NOT NULL,
+  `nama_anggota` varchar(50) NOT NULL,
+  `jenis_kelamin` varchar(20) NOT NULL,
+  `alamat` text NOT NULL,
+  `no_hp` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `anggota`
+--
+
+INSERT INTO `anggota` (`id_anggota`, `nama_anggota`, `jenis_kelamin`, `alamat`, `no_hp`) VALUES
+('AG001', 'Adjie', 'Laki-laki', 'Perumnas Ponjen', '082222222222'),
+('AG002', 'Bagus', 'Laki-laki', 'Kencong', '089999999999'),
+('AG003', 'Layla', 'Perempuan', 'Bagorejo', '081111111111'),
+('AG004', 'Adi', 'Laki-laki', 'Gumukmas', '083333333333');
 
 -- --------------------------------------------------------
 
@@ -46,6 +70,12 @@ INSERT INTO `login` (`id`, `nama`, `username`, `password`, `level`) VALUES
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `anggota`
+--
+ALTER TABLE `anggota`
+  ADD PRIMARY KEY (`id_anggota`);
 
 --
 -- Indeks untuk tabel `login`
